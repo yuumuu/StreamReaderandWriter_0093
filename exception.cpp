c836fa7a797eadc4bdb3756c20706a03d7798d07
yuumuu <haidar.alhazen93@gmail.com>
2025-06-10 15:05:53 +0700

    Membuat Blok Kode Try Catch

--- exception.cpp
+++ exception.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main() {
+    cout << "===============Awal Program===============" << endl;
+    try {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(2) << endl;
+    } catch (...) {
+
+    }
+
     return 0;
 }
