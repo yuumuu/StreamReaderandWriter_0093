b982f71d0288f32672eb0ac0f6b067c0f6bfda9c
yuumuu <haidar.alhazen93@gmail.com>
2025-06-10 15:19:36 +0700

    Ngerapiin Kode ManualException

--- manualException.cpp
+++ manualException.cpp
@@ -8,11 +8,12 @@ int main() {
         cout << "Baris ini tidak akan dieksekusi" << endl;
     } catch (int a) {
         // blok ini akan dieksekusi jika exception bertipe int
-        cout << "Terjadi kesalahan: Baris ini dieksekusi -> exception bertipe int" << endl;
+        cout << "Terjadi kesalahan: Baris ini dieksekusi" << endl;
+        cout << "-> Exception (throw) bertipe int" << endl;
     } catch (...) {
         // blok ini akan dieksekusi jika exception bertipe selain int
-        cout << "Terjadi kesalahan: Baris ini dieksekusi -> exception bertipe selain int" << endl;
-
+        cout << "Terjadi kesalahan: Baris ini dieksekusi" << endl;
+        cout << "-> Exception (throw) bertipe selain int" << endl;
     }
 
     return 0;
