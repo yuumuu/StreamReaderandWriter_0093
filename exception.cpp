6e02a623052b650db0208563a849786d68321910
yuumuu <haidar.alhazen93@gmail.com>
2025-06-10 15:09:39 +0700

    Ubah Akses Array untuk Menguji Exception

--- exception.cpp
+++ exception.cpp
@@ -8,7 +8,7 @@ int main() {
     try {
         array<int, 3> data = {10, 20, 30};
         // pesan array integer 3 elemen
-        cout << data.at(2) << endl;
+        cout << data.at(5) << endl;
     } catch (exception& e) {
         cout << "Terjadi kesalahan: " << e.what() << endl;
     }
