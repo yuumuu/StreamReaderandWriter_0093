3c9e9dfc61bdbf4368054506c5ec6a33c7083b6d
yuumuu <haidar.alhazen93@gmail.com>
2025-06-10 15:12:23 +0700

    Biar Enak Outputnya

--- exception.cpp
+++ exception.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 int main() {
-    cout << "===============Awal Program===============" << endl;
+    cout << "=============== Awal Program ===============" << endl << endl;
     try {
         array<int, 3> data = {10, 20, 30};
-        // pesan array integer 3 elemen
+        
+        cout << "Mengakses Array ke-" << 5 << ": " << endl;
         cout << data.at(5) << endl;
     } catch (exception& e) {
         cout << "Terjadi kesalahan: " << e.what() << endl;
     }
-    cout << "===============Akhir Program===============" << endl;
+    cout << endl << "=============== Akhir Program ===============" << endl;
 
     return 0;
 }
