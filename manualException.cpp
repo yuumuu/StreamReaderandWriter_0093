f7744dbab3c814b07bf54e095f0f722a5e730a17
yuumuu <haidar.alhazen93@gmail.com>
2025-06-10 15:17:33 +0700

    Mengisi Blok Kode Try Catch

--- manualException.cpp
+++ manualException.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 int main() {
     try {
-
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 10;
+        cout << "Baris ini tidak akan dieksekusi" << endl;
     } catch (int a) {
-
+        // blok ini akan dieksekusi jika exception bertipe int
+        cout << "Terjadi kesalahan: Baris ini dieksekusi -> exception bertipe int" << endl;
     } catch (...) {
-        
+        // blok ini akan dieksekusi jika exception bertipe selain int
+        cout << "Terjadi kesalahan: Baris ini dieksekusi -> exception bertipe selain int" << endl;
+
     }
 
     return 0;
